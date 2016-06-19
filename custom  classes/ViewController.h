2b79585dcce8eedb17e2f411052858472841811a
Mandeep Singh <mandeepsingh@mandeeps-macbook-air.local>
2016-06-19 13:19:02 +0530

    Initial Commit

---
+++ custom  classes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  custom  classes
+//
+//  Created by Mandeep Singh on 19/06/16.
+//  Copyright © 2016 Mandeep Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ custom  classes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  custom  classes
+//
+//  Created by Mandeep Singh on 19/06/16.
+//  Copyright © 2016 Mandeep Singh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
